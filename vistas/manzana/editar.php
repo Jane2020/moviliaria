b3e482997b7485c81353c31096af6a36ceb46842
Jane <lajane2020@hotmail.com>
2017-02-01 23:32:23 -0500

    Inclusión de cambios en el modulo de manzanas

--- vistas/manzana/editar.php
+++ vistas/manzana/editar.php
@@ -1,34 +1,47 @@
 <?php
 require("../../modulos/ManzanaModulo.php");
-include "../../template/header.php";
 
 $manzana = new Manzana();
 $item= $manzana->editarManzana();
+$title = (($item->id>0)?'Editar ':'Nueva ').'Manzana';
+require_once ("../../template/header.php");
 
 if (isset($_POST['guardar'])){
-	$multa->guardarMulta();	
+	$manzana->guardarManzana();	
 }
 ?>
+<header class="page-header">
+	<h1 class="page-title"><?php echo $title; ?></h1>
+</header>
 <form id="frmLotizacion" method="post" action="">
 <div style="overflow: auto;">
-	<div class="form-group col-sm-6">
-		<label class="control-label">Nombre</label>
-		<input type='text' name='nombre' class='form-control' value="<?php echo $item->nombre; ?>" id="nombre">
-	</div>
-	<div class="form-group col-sm-6">
-		<label class="control-label">Descripción</label> 
-		<textarea name='descripcion' class='form-control' id="descripcion" rows="5" cols="10">
-			<?php echo $item->descripcion; ?>
-		</textarea>
-	</div>
-	<div class="form-group col-sm-6">
-		<label class="control-label">Valor</label> 
-		<input type='text' name='valor' class='form-control' value="<?php echo $item->valor; ?>" id="valor">
-	</div>
+	<div class="form-group col-sm-12">
+		<div class="form-group col-sm-6">
+			<label class="control-label">Nombre</label>
+			<input type='text' name='nombre' class='form-control' value="<?php echo $item->nombre; ?>" id="nombre">
+		</div>
+	</div>	
+	<div class="form-group col-sm-12">	
+		<div class="form-group col-sm-6">
+			<label class="control-label">Descripción</label> 
+			<textarea name='descripcion' class='form-control' id="descripcion" rows="5" cols="10">
+				<?php echo isset($item->descripcion)?$item->descripcion:null; ?>
+			</textarea>
+		</div>
+	</div>	
+	<div class="form-group col-sm-12">	
+		<div class="form-group col-sm-6">
+			<label class="control-label">Lotización</label> 
+			<input type='text' name='valor' class='form-control' value="<?php echo $item->lotizacion; ?>" id="valor">
+		</div>
+	</div>	
 	<div class="form-group">
-		<input type='hidden' name='id' class='form-control' value="<?php echo $item->id; ?>">		
-		<input type='hidden' name='guardar' value="1">
-		<button type="submit" name="boton" class="btn btn-default">Guardar</button>		
+		<div class="form-group col-sm-6">
+			<input type='hidden' name='id' class='form-control' value="<?php echo $item->id; ?>">		
+			<input type='hidden' name='guardar' value="1">
+			<button type="submit" name="boton" class="btn btn-success btn-sm">Guardar</button>		
+			<a href="listar.php" class="btn btn-info btn-sm">Cancelar</a>
+		</div>		
 	</div>
 </div>
 </form>
--- vistas/manzana/listar.php
+++ vistas/manzana/listar.php
@@ -4,20 +4,19 @@ require("../../modulos/ManzanaModulo.php");
 require_once ("../../template/header.php");
 ?>
 <header class="page-header">
-					<h1 class="page-title"><?php echo $title; ?></h1>
+	<h1 class="page-title"><?php echo $title; ?></h1>
 </header>
 <p>
-   <a href="editar.php" class="btn btn-primary btn-md"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Agregar Multa</a><br/>
+   <a href="editar.php" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-plus" aria-hidden="true" title="Nuevo"></span></a><br/>
 </p>
 <table id="ghatable" class="display table table-bordered table-stripe" cellspacing="0" width="100%">
 	<thead>
           <tr>
                <th>ID</th>
-               <th>NOMBRE</th>
+               <th>Nombre</th>
                <th>Descripción</th>
-               <th>VALOR</th>
-               <th>MODIFICAR</th>
-               <th>ELIMINAR</th>
+               <th>Lotización</th>
+               <th>Acción</th>               
           </tr>
      </thead>
      <tbody>
@@ -32,11 +31,9 @@ require_once ("../../template/header.php");
             <td><?php echo $row->nombre ?></td>
             <td><?php echo $row->descripcion ?></td>
             <td><?php echo $row->lotizacion ?></td>
-            <td>
-                <a href="editar.php?id=<?php echo $row->id ?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</a>
-            </td>
-            <td>
-                <a onclick="return confirm('Desea eliminar el registro')" href="accion.php?id=<?php echo $row->id ?>"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Eliminar</a>
+             <td style="text-align: center;">
+                <a title="Editar" href="editar.php?id=<?php echo $row->id ?>" class="btn btn-warning btn-sm"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> </a>
+                <a title="Eliminar" class="btn btn-danger btn-sm" onclick="return confirm('Desea eliminar el registro')" href="accion.php?id=<?php echo $row->id ?>"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
             </td>
           </tr>
       <?php
