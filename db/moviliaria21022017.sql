046fa67427c1352bb15c142affda3209de8c6340
Jane <lajane2020@hotmail.com>
2017-02-21 22:28:46 -0500

    Nueva bd

--- db/moviliaria21022017.sql
+++ db/moviliaria21022017.sql
@@ -15,11 +15,11 @@
 
 
 --
--- Create schema movliaria
+-- Create schema moviliaria
 --
 
-CREATE DATABASE IF NOT EXISTS movliaria;
-USE movliaria;
+CREATE DATABASE IF NOT EXISTS moviliaria;
+USE moviliaria;
 
 --
 -- Definition of table `acuerdo`
@@ -38,8 +38,6 @@ CREATE TABLE `acuerdo` (
   PRIMARY KEY (`id`),
   KEY `fk_venta_usuario1` (`usuario_id`),
   KEY `fk_venta_lote1` (`lote_id`),
-  KEY `fk_acuerdo_tipo_acuerdo1` (`tipo_acuerdo_id`),
-  CONSTRAINT `fk_acuerdo_tipo_acuerdo1` FOREIGN KEY (`tipo_acuerdo_id`) REFERENCES `tipo_acuerdo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_venta_lote1` FOREIGN KEY (`lote_id`) REFERENCES `lote` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_venta_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -82,7 +80,6 @@ CREATE TABLE `lote` (
   `nombre` varchar(45) NOT NULL,
   `ubicacion` varchar(512) NOT NULL,
   `dimension` varchar(45) DEFAULT NULL,
-  `imagen` varchar(512) DEFAULT NULL,
   `numero_lote` int(11) NOT NULL,
   `disponible` tinyint(4) NOT NULL DEFAULT '1',
   `eliminado` int(11) NOT NULL,
@@ -90,13 +87,15 @@ CREATE TABLE `lote` (
   PRIMARY KEY (`id`),
   KEY `fk_lote_manzana1` (`manzana_id`),
   CONSTRAINT `fk_lote_manzana1` FOREIGN KEY (`manzana_id`) REFERENCES `manzana` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lote`
 --
 
 /*!40000 ALTER TABLE `lote` DISABLE KEYS */;
+INSERT INTO `lote` (`id`,`nombre`,`ubicacion`,`dimension`,`numero_lote`,`disponible`,`eliminado`,`manzana_id`) VALUES 
+ (1,'Lote1','wl','lwl',3,1,0,1);
 /*!40000 ALTER TABLE `lote` ENABLE KEYS */;
 
 
@@ -134,18 +133,27 @@ CREATE TABLE `lote_multa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lote_id` int(11) NOT NULL,
   `multa_id` int(11) NOT NULL,
+  `valor_multa` double NOT NULL,
+  `fecha_ingreso` datetime NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_lote_multa_lote1` (`lote_id`),
   KEY `fk_lote_multa_multa1` (`multa_id`),
   CONSTRAINT `fk_lote_multa_lote1` FOREIGN KEY (`lote_id`) REFERENCES `lote` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_lote_multa_multa1` FOREIGN KEY (`multa_id`) REFERENCES `multa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lote_multa`
 --
 
 /*!40000 ALTER TABLE `lote_multa` DISABLE KEYS */;
+INSERT INTO `lote_multa` (`id`,`lote_id`,`multa_id`,`valor_multa`,`fecha_ingreso`,`descripcion`,`eliminado`) VALUES 
+ (1,1,1,5,'2017-02-20 00:00:00','sdds',0),
+ (2,1,1,5,'2017-02-20 00:00:00','sdds',1),
+ (3,1,1,5,'2017-02-20 00:00:00','sdllsd',1),
+ (4,1,2,200,'2017-02-21 00:00:00','kskksks',0);
 /*!40000 ALTER TABLE `lote_multa` ENABLE KEYS */;
 
 
@@ -170,13 +178,9 @@ CREATE TABLE `lotizacion` (
 
 /*!40000 ALTER TABLE `lotizacion` DISABLE KEYS */;
 INSERT INTO `lotizacion` (`id`,`nombre`,`ciudad`,`sector`,`referencia`,`eliminado`) VALUES 
- (1,'k','k','k','k',0),
- (2,'kks','lsls','lsl','lsls',0),
- (3,'kk','kk','k','k',0),
- (4,'hhh','hh','h','h',0),
- (5,'ask','skak','skska','sksk',0),
- (6,'','kk','kk','1111',0),
- (7,'aa','a','a1','a11222',0);
+ (5,'kkekw','kkewk','wekewk','ewlew',1),
+ (6,'kkekw','kkewk','wekewk','ewlew',1),
+ (7,'skdk','ksdkdks','dsdsl','dllds',0);
 /*!40000 ALTER TABLE `lotizacion` ENABLE KEYS */;
 
 
@@ -188,18 +192,21 @@ DROP TABLE IF EXISTS `manzana`;
 CREATE TABLE `manzana` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(512) NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
   `eliminado` int(11) NOT NULL DEFAULT '0',
   `lotizacion_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_manzana_lotizacion1` (`lotizacion_id`),
   CONSTRAINT `fk_manzana_lotizacion1` FOREIGN KEY (`lotizacion_id`) REFERENCES `lotizacion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `manzana`
 --
 
 /*!40000 ALTER TABLE `manzana` DISABLE KEYS */;
+INSERT INTO `manzana` (`id`,`nombre`,`descripcion`,`eliminado`,`lotizacion_id`) VALUES 
+ (1,'m1','kdskds',0,7);
 /*!40000 ALTER TABLE `manzana` ENABLE KEYS */;
 
 
@@ -212,15 +219,19 @@ CREATE TABLE `multa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `valor` double NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
   `eliminado` varchar(45) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `multa`
 --
 
 /*!40000 ALTER TABLE `multa` DISABLE KEYS */;
+INSERT INTO `multa` (`id`,`nombre`,`valor`,`descripcion`,`eliminado`) VALUES 
+ (1,'Multa 1',100,'dk','0'),
+ (2,'Multa 2',200,'dk','0');
 /*!40000 ALTER TABLE `multa` ENABLE KEYS */;
 
 
@@ -232,16 +243,48 @@ DROP TABLE IF EXISTS `obras_infraestructura`;
 CREATE TABLE `obras_infraestructura` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(128) NOT NULL,
-  `eliminado` int(11) NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `obras_infraestructura`
 --
 
 /*!40000 ALTER TABLE `obras_infraestructura` DISABLE KEYS */;
+INSERT INTO `obras_infraestructura` (`id`,`nombre`,`descripcion`,`eliminado`) VALUES 
+ (1,'Obra 1','sa',0);
 /*!40000 ALTER TABLE `obras_infraestructura` ENABLE KEYS */;
+
+
+--
+-- Definition of table `pago`
+--
+
+DROP TABLE IF EXISTS `pago`;
+CREATE TABLE `pago` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `monto_total` double NOT NULL,
+  `numero_abonos` int(11) NOT NULL,
+  `monto_pagado` double NOT NULL,
+  `estado` int(11) NOT NULL,
+  `acuerdo_id` int(11) NOT NULL,
+  `tipo_pago_id` int(11) NOT NULL,
+  `id_item` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pago_acuerdo1` (`acuerdo_id`),
+  KEY `fk_pago_tipo_pago1` (`tipo_pago_id`),
+  CONSTRAINT `fk_pago_acuerdo1` FOREIGN KEY (`acuerdo_id`) REFERENCES `acuerdo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pago_tipo_pago1` FOREIGN KEY (`tipo_pago_id`) REFERENCES `tipo_pago` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pago`
+--
+
+/*!40000 ALTER TABLE `pago` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pago` ENABLE KEYS */;
 
 
 --
@@ -294,9 +337,8 @@ CREATE TABLE `tipo_acuerdo` (
 
 DROP TABLE IF EXISTS `tipo_pago`;
 CREATE TABLE `tipo_pago` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `nombre` varchar(512) NOT NULL,
   `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -336,19 +378,13 @@ CREATE TABLE `tipo_usuario` (
 DROP TABLE IF EXISTS `transaccion`;
 CREATE TABLE `transaccion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(512) NOT NULL,
-  `valor_recibido` double NOT NULL,
-  `valor_residual` double NOT NULL,
-  `valor_total` double NOT NULL,
-  `operacion_id` int(11) NOT NULL,
+  `fecha_transaccion` datetime NOT NULL,
+  `valor` double NOT NULL,
   `eliminado` int(11) NOT NULL DEFAULT '0',
-  `acuerdo_id` int(11) NOT NULL,
-  `tipo_pago_id` int(11) NOT NULL,
+  `pago_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_transaccion_acuerdo1` (`acuerdo_id`),
-  KEY `fk_transaccion_tipo_pago1` (`tipo_pago_id`),
-  CONSTRAINT `fk_transaccion_acuerdo1` FOREIGN KEY (`acuerdo_id`) REFERENCES `acuerdo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaccion_tipo_pago1` FOREIGN KEY (`tipo_pago_id`) REFERENCES `tipo_pago` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_transaccion_pago1` (`pago_id`),
+  CONSTRAINT `fk_transaccion_pago1` FOREIGN KEY (`pago_id`) REFERENCES `pago` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
