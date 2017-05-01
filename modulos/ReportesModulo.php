c82295babe72fd6720c87c6af2a4cdebb87aeb55
Jane <lajane2020@hotmail.com>
2017-05-01 16:29:48 -0500

    Cambios en estilo de reporte

--- modulos/ReportesModulo.php
+++ modulos/ReportesModulo.php
@@ -109,7 +109,7 @@ class Reportes extends Conexion {
           if(count($data1) > 0){
                foreach ($data1 as $fila){             
 					$html .="    <tr>
-            						<td colspan='7' align='center'><b>".strtoupper($fila->manzana)."</b></td>
+            						<td colspan='7' align='center' style='background-color:yellow'><b>".strtoupper($fila->manzana)."</b></td>
         						</tr>";
               foreach ($fila->lotes as $lote){               	
               	$html .="    	<tr>
@@ -141,5 +141,34 @@ class Reportes extends Conexion {
 		$canvas->page_text(550, 750, "Pág. {PAGE_NUM}/{PAGE_COUNT}", $font, 6, array(0,0,0)); //header
 		$canvas->page_text(270, 770, "Copyright © 2017", $font, 6, array(0,0,0)); //footer			
 		$dompdf->stream('general', array("Attachment"=>false));
-	}	
+	}
+	
+	/**
+	 * Función que obtiene el Listado de Clientes
+	 */
+	public function listarClientesLotización(){
+		$manzanas = $this->mysqli->query("SELECT distinct(m.id) as manzana_id, m.nombre as manzana, null as lotes
+					FROM acuerdo a
+					INNER JOIN lote l ON a.lote_id=l.id
+					INNER JOIN manzana m ON l.manzana_id=m.id");
+		if($manzanas != null){
+			while( $fila = $manzanas->fetch_object() ){
+				$data=[];
+				$lotes = $this->mysqli->query("SELECT a.id , u.nombres, u.apellidos,u.cedula,l.numero_lote, a.valor_total,a.cod_promesa
+													FROM acuerdo a
+													INNER JOIN usuario u ON a.usuario_id=u.id
+													INNER JOIN lote l ON a.lote_id=l.id
+													INNER JOIN manzana m ON l.manzana_id=m.id
+													WHERE m.id=".$fila->manzana_id);
+				if($lotes != null){
+					while( $fila1 = $lotes->fetch_object() ){
+						$data[]= $fila1;
+					}
+				}
+				$fila->lotes = $data;
+				$data1[] = $fila;
+			}
+		}
+		return $data1;
+	}
 }
