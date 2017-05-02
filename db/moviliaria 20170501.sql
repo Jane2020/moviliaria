4ede9f1b56af19fd97e40e66a54eadaa8c52ac52
Jane <lajane2020@hotmail.com>
2017-05-01 19:15:53 -0500

    Inclusión de la base de datos con cambios

--- db/moviliaria 20170501.sql
+++ db/moviliaria 20170501.sql
@@ -35,21 +35,22 @@ CREATE TABLE `acuerdo` (
   `eliminado` int(11) NOT NULL DEFAULT '0',
   `usuario_id` int(11) NOT NULL,
   `lote_id` int(11) NOT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `estado` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_venta_usuario1` (`usuario_id`),
   KEY `fk_venta_lote1` (`lote_id`),
   CONSTRAINT `fk_venta_lote1` FOREIGN KEY (`lote_id`) REFERENCES `lote` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_venta_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `acuerdo`
 --
 
 /*!40000 ALTER TABLE `acuerdo` DISABLE KEYS */;
-INSERT INTO `acuerdo` (`id`,`fecha_ingreso`,`valor_total`,`valor_inicial`,`cod_promesa`,`eliminado`,`usuario_id`,`lote_id`) VALUES 
- (18,'2017-04-05',8000,8000,'wqi',0,2,12),
- (19,'2017-04-05',8000,1000,'22',0,2,13);
+INSERT INTO `acuerdo` (`id`,`fecha_ingreso`,`valor_total`,`valor_inicial`,`cod_promesa`,`eliminado`,`usuario_id`,`lote_id`,`tipo`,`estado`) VALUES 
+ (28,'2017-05-01',20000,20000,'2013-06-01-03',0,4,17,1,1);
 /*!40000 ALTER TABLE `acuerdo` ENABLE KEYS */;
 
 
@@ -70,7 +71,7 @@ CREATE TABLE `lote` (
   PRIMARY KEY (`id`),
   KEY `fk_lote_manzana1` (`manzana_id`),
   CONSTRAINT `fk_lote_manzana1` FOREIGN KEY (`manzana_id`) REFERENCES `manzana` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lote`
@@ -78,9 +79,12 @@ CREATE TABLE `lote` (
 
 /*!40000 ALTER TABLE `lote` DISABLE KEYS */;
 INSERT INTO `lote` (`id`,`nombre`,`ubicacion`,`dimension`,`numero_lote`,`disponible`,`eliminado`,`manzana_id`) VALUES 
- (12,'Lote 1','KSK','2',1,0,0,3),
- (13,'Lote 2','28','28',2,0,0,3),
- (14,'Lote 3','34','334',3,1,0,3);
+ (17,'Lote  1','Ubicación 1','200',1,0,0,6),
+ (18,'Lote 2','Ubicación 2','300',2,1,0,6),
+ (19,'Lote 3','Ubicación 3','300',3,1,0,6),
+ (20,'Lote 4','Ubicación 4','400',4,1,0,7),
+ (21,'Lote 5','Ubicación 5','500',5,1,0,7),
+ (22,'Lote 6','Ubicación 6','600',6,1,0,9);
 /*!40000 ALTER TABLE `lote` ENABLE KEYS */;
 
 
@@ -102,7 +106,7 @@ CREATE TABLE `lote_infraestructura` (
   KEY `fk_lote_infraestructura_lote1` (`lote_id`),
   CONSTRAINT `fk_lote_infraestructura_infraestructura1` FOREIGN KEY (`infraestructura_id`) REFERENCES `obras_infraestructura` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_lote_infraestructura_lote1` FOREIGN KEY (`lote_id`) REFERENCES `lote` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lote_infraestructura`
@@ -110,8 +114,9 @@ CREATE TABLE `lote_infraestructura` (
 
 /*!40000 ALTER TABLE `lote_infraestructura` DISABLE KEYS */;
 INSERT INTO `lote_infraestructura` (`id`,`valor`,`fecha_ingreso`,`estado`,`eliminado`,`infraestructura_id`,`lote_id`) VALUES 
- (3,600,'2017-04-01',NULL,0,2,12),
- (4,400,'2017-04-01',NULL,0,1,13);
+ (7,500,'2017-05-01',NULL,0,3,17),
+ (8,1000,'2017-05-01',NULL,0,4,17),
+ (9,800,'2017-05-01',NULL,0,5,17);
 /*!40000 ALTER TABLE `lote_infraestructura` ENABLE KEYS */;
 
 
@@ -134,7 +139,7 @@ CREATE TABLE `lote_multa` (
   KEY `fk_lote_multa_multa1` (`multa_id`),
   CONSTRAINT `fk_lote_multa_lote1` FOREIGN KEY (`lote_id`) REFERENCES `lote` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_lote_multa_multa1` FOREIGN KEY (`multa_id`) REFERENCES `multa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lote_multa`
@@ -142,12 +147,7 @@ CREATE TABLE `lote_multa` (
 
 /*!40000 ALTER TABLE `lote_multa` DISABLE KEYS */;
 INSERT INTO `lote_multa` (`id`,`lote_id`,`multa_id`,`valor_multa`,`fecha_ingreso`,`descripcion`,`estado`,`eliminado`) VALUES 
- (1,12,3,2000,'2017-04-01 00:00:00','A',NULL,0),
- (2,13,3,2000,'2017-04-01 00:00:00','A',NULL,0),
- (3,12,3,2000,'2017-04-08 00:00:00','mk',NULL,0),
- (4,13,4,4000,'2017-04-10 00:00:00','kkk',NULL,0),
- (5,12,3,2000,'2017-04-11 00:00:00','k',NULL,0),
- (6,13,3,2000,'2017-04-11 00:00:00','k',NULL,0);
+ (1,17,5,500,'2017-05-01 00:00:00','Multa',NULL,0);
 /*!40000 ALTER TABLE `lote_multa` ENABLE KEYS */;
 
 
@@ -164,7 +164,7 @@ CREATE TABLE `lotizacion` (
   `referencia` varchar(1024) DEFAULT NULL,
   `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lotizacion`
@@ -172,7 +172,7 @@ CREATE TABLE `lotizacion` (
 
 /*!40000 ALTER TABLE `lotizacion` DISABLE KEYS */;
 INSERT INTO `lotizacion` (`id`,`nombre`,`ciudad`,`sector`,`referencia`,`eliminado`) VALUES 
- (9,'Lotización 1','Riobamba','Los Pinos','Los Pinos',0);
+ (10,'Donovilsa I','Riobamba','La Dolorosa I','La Dolorosa I',0);
 /*!40000 ALTER TABLE `lotizacion` ENABLE KEYS */;
 
 
@@ -190,7 +190,7 @@ CREATE TABLE `manzana` (
   PRIMARY KEY (`id`),
   KEY `fk_manzana_lotizacion1` (`lotizacion_id`),
   CONSTRAINT `fk_manzana_lotizacion1` FOREIGN KEY (`lotizacion_id`) REFERENCES `lotizacion` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `manzana`
@@ -198,8 +198,10 @@ CREATE TABLE `manzana` (
 
 /*!40000 ALTER TABLE `manzana` DISABLE KEYS */;
 INSERT INTO `manzana` (`id`,`nombre`,`descripcion`,`eliminado`,`lotizacion_id`) VALUES 
- (3,'Manzana 1','asl',0,9),
- (4,'Manzana 2','askask',0,9);
+ (6,'Manzana A','Manzana A',0,10),
+ (7,'Manzana B','Manzana B',0,10),
+ (8,'Manzana C','Manzana C',1,10),
+ (9,'Manzana C','Manzana C',0,10);
 /*!40000 ALTER TABLE `manzana` ENABLE KEYS */;
 
 
@@ -215,7 +217,7 @@ CREATE TABLE `multa` (
   `descripcion` varchar(512) DEFAULT NULL,
   `eliminado` varchar(45) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `multa`
@@ -223,8 +225,7 @@ CREATE TABLE `multa` (
 
 /*!40000 ALTER TABLE `multa` DISABLE KEYS */;
 INSERT INTO `multa` (`id`,`nombre`,`valor`,`descripcion`,`eliminado`) VALUES 
- (3,'Multa Uno',2000,'dsl','0'),
- (4,'Multa Dos',4000,'Mull','0');
+ (5,'Multa 1',500,'Multa 1','0');
 /*!40000 ALTER TABLE `multa` ENABLE KEYS */;
 
 
@@ -240,7 +241,7 @@ CREATE TABLE `obras_infraestructura` (
   `descripcion` varchar(512) DEFAULT NULL,
   `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `obras_infraestructura`
@@ -248,8 +249,9 @@ CREATE TABLE `obras_infraestructura` (
 
 /*!40000 ALTER TABLE `obras_infraestructura` DISABLE KEYS */;
 INSERT INTO `obras_infraestructura` (`id`,`nombre`,`valor`,`descripcion`,`eliminado`) VALUES 
- (1,'Obra Uno',400,'Obra',0),
- (2,'Obra Dos',600,'Obra',0);
+ (3,'Agua, Luz',500,'Servicios Básicos',0),
+ (4,'Alcantarillado',1000,'Alcantarillado',0),
+ (5,'Bordillos y Aceras',800,'Aceras',0);
 /*!40000 ALTER TABLE `obras_infraestructura` ENABLE KEYS */;
 
 
@@ -271,7 +273,7 @@ CREATE TABLE `pago` (
   KEY `fk_pago_acuerdo1` (`acuerdo_id`),
   KEY `fk_acuerdo_id` (`acuerdo_id`),
   CONSTRAINT `fk_acuerdo_id` FOREIGN KEY (`acuerdo_id`) REFERENCES `acuerdo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pago`
@@ -279,12 +281,11 @@ CREATE TABLE `pago` (
 
 /*!40000 ALTER TABLE `pago` DISABLE KEYS */;
 INSERT INTO `pago` (`id`,`monto_total`,`numero_abonos`,`monto_pagado`,`estado`,`acuerdo_id`,`id_item`,`id_obra_multa`) VALUES 
- (25,8000,1,8000,1,18,1,0),
- (26,8000,4,8000,1,19,1,0),
- (27,2000,1,2000,1,18,3,3),
- (28,4000,1,4000,1,19,3,4),
- (29,2000,1,-10212,1,18,3,5),
- (30,2000,1,1300,2,19,3,6);
+ (41,20000,1,20000,1,28,1,0),
+ (42,500,1,200,2,28,2,7),
+ (43,1000,1,0,0,28,2,8),
+ (44,800,1,0,0,28,2,9),
+ (45,500,1,400,2,28,3,1);
 /*!40000 ALTER TABLE `pago` ENABLE KEYS */;
 
 
@@ -351,7 +352,7 @@ CREATE TABLE `transaccion` (
   PRIMARY KEY (`id`),
   KEY `fk_transaccion_pago1` (`pago_id`),
   CONSTRAINT `fk_transaccion_pago1` FOREIGN KEY (`pago_id`) REFERENCES `pago` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaccion`
@@ -359,33 +360,9 @@ CREATE TABLE `transaccion` (
 
 /*!40000 ALTER TABLE `transaccion` DISABLE KEYS */;
 INSERT INTO `transaccion` (`id`,`fecha_transaccion`,`monto_total`,`valor`,`pago_id`,`tipo_pago_id`,`eliminado`) VALUES 
- (1,'2017-04-05 00:00:00',0,8000,25,1,0),
- (2,'2017-04-05 00:00:00',8000,1000,26,2,0),
- (4,'2017-04-10 00:00:00',0,1000,27,2,0),
- (5,'2017-04-10 00:00:00',0,1000,27,1,0),
- (6,'2017-04-10 00:00:00',7000,1000,26,2,0),
- (7,'2017-04-10 00:00:00',5000,2000,26,2,0),
- (8,'2017-04-10 00:00:00',4000,1000,26,2,0),
- (9,'2017-04-10 00:00:00',3000,1000,26,2,0),
- (10,'2017-04-10 00:00:00',4000,1000,28,2,0),
- (11,'2017-04-10 00:00:00',3000,1000,28,2,0),
- (12,'2017-04-10 00:00:00',2000,1000,28,2,0),
- (13,'2017-04-10 00:00:00',1000,1000,28,2,0),
- (14,'2017-04-11 00:00:00',2000,500,30,2,0),
- (15,'2017-04-11 00:00:00',1500,500,30,2,0),
- (16,'2017-04-13 00:00:00',2000,500,29,2,0),
- (17,'2017-04-13 00:00:00',1500,500,29,2,0),
- (18,'2017-04-14 00:00:00',1000,1,29,2,0),
- (19,'2017-04-14 00:00:00',999,1,29,2,0),
- (20,'2017-04-14 00:00:00',998,10,29,2,0),
- (21,'2017-04-14 00:00:00',988,100,29,2,0),
- (22,'2017-04-14 00:00:00',888,100,29,2,0),
- (23,'2017-04-14 00:00:00',788,1000,29,2,0),
- (24,'2017-04-14 00:00:00',-212,10000,29,2,0),
- (25,'2017-04-14 00:00:00',-10212,-10212,29,1,0),
- (26,'2017-04-14 00:00:00',1000,100,30,2,0),
- (27,'2017-04-14 00:00:00',900,100,30,2,0),
- (28,'2017-04-14 00:00:00',800,100,30,2,0);
+ (44,'2017-05-01 00:00:00',20000,20000,41,1,0),
+ (45,'2017-05-01 00:00:00',500,200,42,2,0),
+ (46,'2017-05-01 00:00:00',500,400,45,2,0);
 /*!40000 ALTER TABLE `transaccion` ENABLE KEYS */;
 
 
@@ -402,21 +379,23 @@ CREATE TABLE `usuario` (
   `password` varchar(45) NOT NULL,
   `email` varchar(128) DEFAULT NULL,
   `celular` varchar(10) DEFAULT NULL,
-  `eliminado` int(11) NOT NULL DEFAULT '0',
+  `telefono` varchar(10) DEFAULT NULL,
+  `direccion` varchar(256) DEFAULT NULL,
   `tipo_usuario_id` int(11) NOT NULL,
+  `eliminado` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_usuario_tipo_usuario1` (`tipo_usuario_id`),
   CONSTRAINT `fk_usuario_tipo_usuario1` FOREIGN KEY (`tipo_usuario_id`) REFERENCES `tipo_usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usuario`
 --
 
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`id`,`cedula`,`nombres`,`apellidos`,`password`,`email`,`celular`,`eliminado`,`tipo_usuario_id`) VALUES 
- (1,'0603108770','Jane','C','202cb962ac59075b964b07152d234b70','l','099999',0,1),
- (2,'0602567802','Ana','Cantos','abc19440e9a4a2b4ea0994db8f4e3a7a','la@g.com','0328999999',0,3);
+INSERT INTO `usuario` (`id`,`cedula`,`nombres`,`apellidos`,`password`,`email`,`celular`,`telefono`,`direccion`,`tipo_usuario_id`,`eliminado`) VALUES 
+ (1,'0603108770','Jane','Concha','202cb962ac59075b964b07152d234b70','lajane2020@hotmail.com','0999999999',NULL,NULL,1,0),
+ (4,'0600034201','Juan','Perez','202cb962ac59075b964b07152d234b70','juan@perez.com','0999999777','0999944444','Los Pinos Altos',3,0);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 
