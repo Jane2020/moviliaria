ac787d02e777e909be646cc2dee8a21731c6b524
fabian <fabian@dell>
2017-05-08 23:54:51 -0500

    Implementacion de Nuestros clientes

--- web/index.php
+++ web/index.php
@@ -84,36 +84,36 @@
 			<!---testimonials--->
 					<div class="testimonials">
 						<div class="container">
-							<h3>testimonial</h3>
+							<h3>Nuestros Clientes</h3>
 							<span></span>
 							<div id="owl-demo" class="owl-carousel">
 								<div class="item">
 									<div class="col-md-2 testmonial-img">
-										<img src="images/t1.png" class="img-responsive" alt=""/>
+										<img src="images/cliente1.jpg" class="img-responsive img-circle" alt="" style="height: 125px; width: 125px;"/>
 									</div>
 									<div class="col-md-10 testmonial-text">
-										<p>Lorem ipsum dolor sit amet, offendit volutpat sea ex, at omnium scripta pro, at omnium scripta pro, ei mea oratio malorum forensibus. ei mea oratio malorum forensibus. Sed ei omnes laoreet posidonium ei mea oratio malorum forensibus.</p>
-										<h4><a href="#">Michael Feng</a></h4>
+										<p>“Encontrar un lote de terreno en la ciudad de Riobamba no es tan fácil por los elevados precios, pero el Proyecto de Urbanización Donovilsa III, nos ayudó a cumplir con este deseo”.</p>
+										<h4><a href="#">Sra. Joselyn Ruiz - Urbanización Donovilsa III</a></h4>
 									</div>
 									<div class="clearfix"> </div>
 								</div>
 								<div class="item">
 									<div class="col-md-2 testmonial-img">
-										<img src="images/t2.png" class="img-responsive" alt=""/>
+										<img src="images/cliente2.jpg" class="img-responsive img-circle" alt="" style="height: 125px; width: 125px;"/>
 									</div>
 									<div class="col-md-10 testmonial-text">
-										<p>Lorem ipsum dolor sit amet, offendit volutpat sea ex, at omnium scripta pro, at omnium scripta pro, ei mea oratio malorum forensibus. ei mea oratio malorum forensibus. Sed ei omnes laoreet posidonium ei mea oratio malorum forensibus.</p>
-										<h4><a href="#">Stacy Barron</a></h4>
+										<p>“Contar con una casa propia es el sueño de toda familia, gracias a la ayuda y facilidad que brinda la Compañía Nuevo Amanecer Donovilsa S.A. lo he logrado”.</p>
+										<h4><a href="#">Lcdo. Javier Nauñay - Urbanización Donovilsa II</a></h4>
 									</div>
 									<div class="clearfix"> </div>
 								</div>
 								<div class="item">
 									<div class="col-md-2 testmonial-img">
-										<img src="images/t3.png" class="img-responsive" alt=""/>
+										<img src="images/cliente3.jpg" class="img-responsive img-circle" alt="" style="height: 125px; width: 125px;"/>
 									</div>
 									<div class="col-md-10 testmonial-text">
-										<p>Lorem ipsum dolor sit amet, offendit volutpat sea ex, at omnium scripta pro, at omnium scripta pro, ei mea oratio malorum forensibus. ei mea oratio malorum forensibus. Sed ei omnes laoreet posidonium ei mea oratio malorum forensibus.</p>
-										<h4><a href="#">Johnson </a></h4>
+										<p>“Formar parte de una Institución seria, con amplia experiencia y trayectoria ya definida, es una gran ventaja para nosotros como clientes, pues estamos seguros que las inversiones efectuadas son con un buen objetivo, tener una vivienda propia”.</p>
+										<h4><a href="#">Lcda. Jimena Parra – Urbanización Donovilsa I</a></h4>
 									</div>
 									<div class="clearfix"> </div>
 								</div>
