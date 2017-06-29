14ccf7f980e469c2295453d0be230a97cfe8f042
Jane <lajane2020@hotmail.com>
2017-06-28 22:05:19 -0500

    Inclusión de páginas de errores

---
+++ vistas/seguridad/error500.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Página No Encontrada</title>
+<link href="<?php echo PATH_CSS . '/bootstrap.min.css';?>"
+	rel="stylesheet">
+</head>
+
+<body class="tooltips">
+	<div style="text-align: center; width: 980px; margin: 100px auto;">
+		<h1>Upps! Página No Encontrada</h1>
+<div class="title-home">Lo sentimos la página que busca ya no existe!. </div>
+		
+<img src="<?php echo PATH_IMAGES. "/404.jpg"; ?>" >
+
+<div class="title-home">Regresar a la <a href="/moviliaria/web">página de inicio</a>.</div>
+</div>
+</body>
+</html>
+
+
+
